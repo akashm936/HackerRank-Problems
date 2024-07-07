a9e9e934d79adaa92bac2b290fd5927fc197fd15
Akash Morwal <akashbhau101@gmail.com>
2024-07-08 01:15:43 +0530

    new problem solve of  HackerRank (08-07-2024)

---
+++ HackerRank2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+
+class HackerRank2
+{
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+		int num1 = scanner.nextInt();
+		double num2 = scanner.nextDouble();
+		String name = sc.nextLine();
+
+		System.out.println("String: "+ name);
+		System.out.println("Double: "+ num2);
+		System.out.println("Int: "+ num1);
+	}
+}
