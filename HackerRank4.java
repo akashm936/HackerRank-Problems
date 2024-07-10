7ea965ee531acf683fa43a8e2126732dcb4e4b27
Akash Morwal <akashbhau101@gmail.com>
2024-07-11 00:50:30 +0530

    new program of hackerRank is upadated (11-07-2024) (

---
+++ HackerRank4.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class HackerRank4 {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("================================");
+        {
+            for(int i=0;i<3;i++)
+            {
+                 String input1 = scanner.next();
+                 int input2 = scanner.nextInt();
+                  System.out.printf(" %-16s %03d\n",input1,input2);
+
+            }
+            // String input1 = scanner.next();
+            // int input2 = scanner.nextInt();
+            // String input3 = scanner.next();
+            // int input4 = scanner.nextInt();
+            // String input5 = scanner.next();
+            // int input6 = scanner.nextInt();
+           
+            // System.out.printf(" %-16s %03d\n",input1,input2);
+            // System.out.printf(" %-16s %03d\n",input1,input2);
+            // System.out.printf(" %-16s %03d\n",input1,input2);
+        }
+        
+        System.out.println("================================");
+    
+    }
+}
